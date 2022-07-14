bfa954e36c86c416965f9b275ac92dc62429eaa0
Leonard18aa <67755595+Leonard18aa@users.noreply.github.com>
2022-07-13 17:45:21 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,66 @@
+function RedNotice(){
+    let movie1 = {
+        id: Number(1),
+        title: 'Red Notice',
+        year: Number(2021),
+        reviews: Number(4.0),
+        description: `An Interpol agent successfully tracks down the world's most wanted art thief, with help from a rival thief. But nothing is as it seems, as a series of double-crosses ensue.`,
+        actors: {
+            DwayneJohnson: 'John Hartley',
+            RyanReynolds: 'Nolan Booth',
+            GalGadot: 'The Bishop',
+        }
+    }
+    console.log(movie1)
+ }
+
+ document.getElementById(RedNotice)
+
+
+function JungleCruise(){
+    let movie2 = {
+        id: Number(2),
+        title: 'Jungle Cruise',
+        year: Number(2021),
+        reviews: Number(6.6),
+        description: `Based on Disneyland's theme park ride where a small riverboat takes a group of travelers through a jungle filled with dangerous animals and reptiles but with a supernatural element.`,
+        actors: {
+            DwayneJohnson: 'Frank Wolff',
+            EmilyBlunt: 'Lily Houghton',
+            EdgarRamírez: 'Aguirre',
+        }
+    }
+    console.log(movie2)
+}
+
+function TheNotebook(){
+    let movie3 = {
+        id: Number(3),
+        title: 'The Notebook',
+        year: Number(2004),
+        reviews: Number(7.8),
+        description: `A poor yet passionate young man (Ryan Gosling) falls in love with a rich young woman (Rachel McAdams), giving her a sense of freedom, but they are soon separated because of their social differences.`,
+        actors: {
+            RachelMcAdams: 'Allie',
+            JamesGarner: 'Duke',
+            RyanGosling: 'Noah',
+        }
+    }
+    console.log(movie3)
+}
+
+function TheFaultinOurStars(){
+    let movie4 = {
+        id: Number(4),
+        title: 'The Fault in Our Stars',
+        year: Number(2014),
+        reviews: Number(7.7),
+        description: `Two teenage cancer patients begin a life-affirming journey to visit a reclusive author in Amsterdam.`,
+        actors: {
+            ShaileneWoodley: 'Hazel',
+            AnselElgort: 'Gus',
+            NatWolff: 'Isaac',
+        }
+    }
+    console.log(movie4)
+}
